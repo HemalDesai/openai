35362ee18abfbf414f933d6d2da0ff20e6b366fa
HemalDesai <hemald332@gmail.com>
2023-06-26 20:47:23 +0530

    first commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from './firebase';
+import { initializeApp } from 'firebase/app';
+import HomePage from '@/pages/HomePage';
+
+
+
+const IndexPage = () => {
+
+  const [user, error] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/LoginPage'); // Redirect to the login page if the user is not logged in
+    }
+  }, [user, router]);
+
+  
+
+  return <HomePage />;
+};
+
+export default IndexPage;
